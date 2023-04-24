fa16eb687d99f61d17c1400aeed478ecdd127c4f
cofucan <coolucheofia@gmail.com>
2023-04-24 02:01:14 +0100

    feat: Function to find file in path

--- ss-check_file_status.c
+++ ss-check_file_status.c
@@ -13,12 +13,29 @@ bool check_file_status(char *pathname, struct stat *statbuf)
 	return (false);
 }
 
-bool check_file_in_path(char *pathname, struct stat *statbuf)
+char *check_file_in_path(char *filename, struct stat *statbuf)
 {
-	size_t no_of_paths;
+	size_t s, no_of_paths;
+	char *full_path;
 	char *current_path = _getenv("PATH");
 	char **paths = split_string(current_path, ":", &no_of_paths);
 
-	return (true);
+	/* Iterate over each paths */
+	for (s = 0; s < no_of_paths; s++)
+	{
+		/* Concatenate the path with the executable name */
+		full_path = strs_concat(3, paths[s], "/", filename);
+
+		if (check_file_status(full_path, statbuf))
+		{
+			free_vector(paths, no_of_paths);
+			return (full_path);
+		}
+	}
+
+	/* If executable doesn't exist in any of the paths */
+	free_vector(paths, no_of_paths);
+	free(full_path);
+	return (NULL);
 }
 
