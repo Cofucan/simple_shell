f6d596710146977333b11579f5991db56e15770c
cofucan <coolucheofia@gmail.com>
2023-04-24 02:02:01 +0100

    chore: Include prototype for function that looks for file in path

--- shell.h
+++ shell.h
@@ -12,6 +12,7 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdarg.h>
 #include <errno.h>
 
 #define MAX_WORD_COUNT 100
@@ -19,7 +20,7 @@
 int execute(char **arguments, int no_of_args, char **envp);
 void handle_error(pid_t pid);
 bool check_file_status(char *pathname, struct stat *statbuf);
-bool check_file_in_path(char *pathname, struct stat *statbuf);
+char *check_file_in_path(char *pathname, struct stat *statbuf);
 char **split_string(char *str, char *delim, size_t *word_count);
 bool is_delimiter(char c, char *delimiters);
 int words_count(char *str, char *delim, unsigned int *arr);
@@ -27,6 +28,7 @@ void free_vector(char **vec, int len);
 void set_zeros(unsigned int *arr, size_t size);
 int _strncmp(char *s1, char *s2, size_t n);
 char *str_concat(char *s1, char *s2);
+char *strs_concat(int args_no, char *s1, char *s2, ...);
 char *_getenv(char *name);
 int _strlen(char *s);
 
